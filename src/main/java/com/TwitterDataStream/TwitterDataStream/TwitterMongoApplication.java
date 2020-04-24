331909c582dd7d8a7197568969af9ae2191925d5
David Wagner <dave.avid@gmail.com>
2020-04-24 12:15:15 -0500

    lets get started

---
+++ src/main/java/com/TwitterDataStream/TwitterDataStream/TwitterMongoApplication.java
@@ -0,0 +1,13 @@
+package com.TwitterDataStream.TwitterDataStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TwitterDataStream/TwitterDataStream/TwitterMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TwitterDataStream.TwitterDataStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
