b1fb210125995bf06208df52021916508750c626
David Wagner <dave.avid@gmail.com>
2020-04-24 13:35:09 -0500

    updated

--- src/test/java/com/TwitterDataStream/mainApplicationTests.java
+++ src/test/java/com/TwitterDataStream/mainApplicationTests.java
@@ -1,10 +1,10 @@
-package com.TwitterDataStream.TwitterDataStream;
+package com.TwitterDataStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TwitterMongoApplicationTests {
+class mainApplicationTests {
 
 	@Test
 	void contextLoads() {
